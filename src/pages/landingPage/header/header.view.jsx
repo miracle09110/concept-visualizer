24a12dc305a96c64fb4a810187e91bd533dea304
james bariuad <114486039+jamesBariuad@users.noreply.github.com>
2024-05-07 17:50:46 +0800

    fix mobile view

--- src/pages/landingPage/header/header.view.jsx
+++ src/pages/landingPage/header/header.view.jsx
@@ -3,23 +3,26 @@ import style from "./header.module.css";
 
 const HeaderView = () => {
   return (
-    <div className={style.header_section}>
+    <>
       <nav>
         <h1>Concept Visualizer</h1>
         <div className={style.hamburger_menu}>=</div>
       </nav>
-      <div className={style.text_container}>
-        <h1>
-          Concept Visualizers: Explore Algorithms, React, and Web Dev Concepts
-        </h1>
-        <h3>
-          Interactive Tools for Understanding Complex Algorithms, ReactJS and
-          Web Development Concepts
-        </h3>
-        <a href="#explore"><button>Explore!</button></a>
-        
+      <div className={style.header_section}>
+        <div className={style.text_container}>
+          <h1>
+            Concept Visualizers: Explore Algorithms, React, and Web Dev Concepts
+          </h1>
+          <h3>
+            Interactive Tools for Understanding Complex Algorithms, ReactJS and
+            Web Development Concepts
+          </h3>
+          <a href="#explore">
+            <button>Explore!</button>
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
