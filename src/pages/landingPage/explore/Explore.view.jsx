4fb3e0207345ec280b64dd4190d86d5a565e61de
james bariuad <114486039+jamesBariuad@users.noreply.github.com>
2024-05-08 18:47:09 +0800

    code cleanup: remove comments, console logs, fixed spacing

--- src/pages/landingPage/explore/Explore.view.jsx
+++ src/pages/landingPage/explore/Explore.view.jsx
@@ -2,18 +2,16 @@ import React from "react";
 import style from "./explore.module.css";
 
 const ExploreView = ({ concepts }) => {
-  console.log(concepts);
-
   const conceptList = concepts.map((concept) => {
     return (
-      <div className={style.card}>
+      <div key={concept.category} className={style.card}>
         <h3 className={style.main_text} key={concept.category}>
           {concept.category}
         </h3>
 
         <div className={style.item_container}>
           {concept.items.map((item) => (
-            <div className={style.item}>
+            <div key={item} className={style.item}>
               <a>{item}</a>
               <div className={style.placeholder}></div>
             </div>
@@ -28,7 +26,6 @@ const ExploreView = ({ concepts }) => {
       <h2>Explore</h2>
 
       {conceptList}
-     
     </div>
   );
 };
