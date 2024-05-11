c6034ff2ad470b7f7d6f944f34cb94b4249d2f78
Isabel Louise Vicente <isabellouiseph@gmail.com>
2024-05-11 22:51:45 +0800

    fixing spacing

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import { resolve, dirname } from 'node:path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-  css: {
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
+	},
+	css: {
 		modules: true, // Enable CSS modules
 	},
 });
