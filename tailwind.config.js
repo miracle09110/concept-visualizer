12e0ef44d859bdffbe066c03dfb05c00533e00cb
abbi-vicente <114486013+abbi-vicente@users.noreply.github.com>
2024-05-11 22:15:56 +0800

    fixed spacing tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
-theme: {
-extend: {
-colors: {
-primary: "#FF76CE",
-secondary: "#FDFFC2",
-tertiary: "#94FFD8",
-quarternary: "#A3D8FF",
-},
-},
-},
-plugins: [],
+ content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
+ theme: {
+  extend: {
+   colors: {
+    primary: "#FF76CE",
+    secondary: "#FDFFC2",
+    tertiary: "#94FFD8",
+    quarternary: "#A3D8FF",
+   },
+  },
+ },
+ plugins: [],
 };
