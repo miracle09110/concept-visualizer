315f0cf63f1a67eb2a0acd10c50bc151965e5645
adrian.taay <adrian.tm@live.com>
2024-05-14 20:32:07 +0800

    bug fixing

--- src/pages/BubbleSort/BubbleSort.view.jsx
+++ src/pages/BubbleSort/BubbleSort.view.jsx
@@ -36,20 +36,18 @@ function BubbleSort() {
   const result = useMemo(() => bubbleSort([...array]), [array]);
 
   function initialize(num = 8) {
-    const random = [...Array(num)].map((_, index) => {
+    const randomArray = [...Array(num)].map((_, index) => {
       return {
         id: index,
         number: Math.floor(Math.random() * 50) - 10,
       };
     });
-    setArray(random);
+    setArray(randomArray);
   }
 
   useEffect(() => {
     initialize();
   }, []);
-
-  console.log(array);
 
   function start() {
     setInd1(0);
@@ -62,28 +60,28 @@ function BubbleSort() {
     const copy = [...array];
 
     // checks if the demo array is arranged exactly as to that of the memoed solution.
-    if (
-      ind1 === copy.length - 2 &&
-      ind2 === copy.length - 1 &&
-      result.every((value, index) => value.number === copy[index].number)
-    ) {
-      setOngoing(false);
-      setComplete(true);
-      setInd1(-1);
-      setInd2(-1);
-      return;
-    }
+    // if (
+    //   ind1 === copy.length - 2 &&
+    //   ind2 === copy.length - 1 &&
+    //   result.every((value, index) => value.number === copy[index].number)
+    // ) {
+    //   setOngoing(false);
+    //   setComplete(true);
+    //   setInd1(-1);
+    //   setInd2(-1);
+    //   return;
+    // }
 
     // checks if ind1 and ind2 are at the end of the array and checks the last two elements if arranged properly. if yes, will iterate over the array again
-    if (
-      ind1 === copy.length - 2 &&
-      ind2 === copy.length - 1 &&
-      copy[ind1].number < copy[ind2].number
-    ) {
-      setInd1(0);
-      setInd2(1);
-      return;
-    }
+    // if (
+    //   ind1 === copy.length - 2 &&
+    //   ind2 === copy.length - 1 &&
+    //   copy[ind1].number < copy[ind2].number
+    // ) {
+    //   setInd1(0);
+    //   setInd2(1);
+    //   return;
+    // }
 
     // checks if left element is smaller than right element. if yes, proceed to next element.
     if (copy[ind1].number <= copy[ind2].number) {
@@ -96,8 +94,8 @@ function BubbleSort() {
     }
     setArray((a) => [...copy]);
   }
-  // console.log(`array: ${array}`, `ind1: ${ind1}`, `ind2: ${ind2}`);
-  // console.log(`result: ${result}`);
+  console.log(`array: ${array}`, `ind1: ${ind1}`, `ind2: ${ind2}`);
+  console.log(`result: ${result}`);
 
   return (
     <section className={style.section}>
