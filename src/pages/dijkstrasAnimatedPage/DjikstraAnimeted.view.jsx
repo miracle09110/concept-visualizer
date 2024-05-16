117e77f0a45fde25cbdd59adead64d6d0883c470
james bariuad <114486039+jamesBariuad@users.noreply.github.com>
2024-05-16 16:27:45 +0800

    added obstacles and grid resizing

--- src/pages/dijkstrasAnimatedPage/DjikstraAnimeted.view.jsx
+++ src/pages/dijkstrasAnimatedPage/DjikstraAnimeted.view.jsx
@@ -7,9 +7,7 @@ const DijkstraAnimatedView = () => {
   const [visitedNodes, setVisitedNodes] = useState(new Set());
   const [shortestPath, setShortestPath] = useState([]);
   const [isAnimatingPath, setIsAnimatingPath] = useState(false);
-  const [dimension, setDimension] = useState(30)
-
-
+  const [dimension, setDimension] = useState(30);
   const createGridArray = (numberOfRows, numberOfColumns) => {
     const grid = [];
     for (let row = 0; row < numberOfRows; row++) {
@@ -21,36 +19,43 @@ const DijkstraAnimatedView = () => {
   };
 
   useEffect(() => {
-    
-
-    
     const grid = createGridArray(dimension, dimension);
     setGridArray(grid);
-  
-
- 
   }, []);
-
-
 
   const dijkstra = () => {
     const graph = {}; // Construct graph with neighbors (adjacency list)
-    for (const item of gridArray) {
-      const [row, col] = item.coordinates;
+    for (const cell of gridArray) {
+      const [row, col] = cell.coordinates;
+      if (cell.type === "obstacle") {
+        graph[`${row}-${col}`] = [];
+        continue; // Skip adding neighbors for obstacles
+      }
       const neighbors = [];
-      if (row > 0) neighbors.push(`${row - 1}-${col}`); // Top neighbor
-      if (row < dimension-1) neighbors.push(`${row + 1}-${col}`); // Bottom neighbor
-      if (col > 0) neighbors.push(`${row}-${col - 1}`); // Left neighbor
-      if (col < dimension-1) neighbors.push(`${row}-${col + 1}`); // Right neighbor
+      const numberOfColumns = dimension;
+      const indexTop = (row - 1) * numberOfColumns + col;
+      const indexBottom = (row + 1) * numberOfColumns + col;
+      const indexLeft = row * numberOfColumns + (col - 1);
+      const indexRight = row * numberOfColumns + (col + 1);
+
+      if (row > 0 && gridArray[indexTop].type !== "obstacle")
+        neighbors.push(`${row - 1}-${col}`); // Top neighbor
+
+      if (row < dimension - 1 && gridArray[indexBottom].type !== "obstacle")
+        neighbors.push(`${row + 1}-${col}`); // Bottom neighbor
+
+      if (col > 0 && gridArray[indexLeft].type !== "obstacle")
+        neighbors.push(`${row}-${col - 1}`); // Left neighbor
+
+      if (col < dimension - 1 && gridArray[indexRight].type !== "obstacle")
+        neighbors.push(`${row}-${col + 1}`); // Right neighbor
+
       graph[`${row}-${col}`] = neighbors;
     }
-
-    console.log(gridArray)
-
     const distances = {}; // Distances from startSquare to each node
     const previous = {}; // Previous node in the shortest path
-    for (const item of gridArray) {
-      const [row, col] = item.coordinates;
+    for (const cell of gridArray) {
+      const [row, col] = cell.coordinates;
       const key = `${row}-${col}`;
       distances[key] =
         key === `${startSquare[0]}-${startSquare[1]}` ? 0 : Infinity;
@@ -58,7 +63,6 @@ const DijkstraAnimatedView = () => {
     }
 
     const unvisited = new Set(Object.keys(distances));
-    console.log(unvisited)
 
     const animateDijkstra = () => {
       const animate = setInterval(() => {
@@ -85,9 +89,9 @@ const DijkstraAnimatedView = () => {
           traceShortestPath(currentNode); // Trigger path tracing
         }
 
-        for (const neighbor of graph[currentNode]) {
-          const [row, col] = neighbor.split("-");
-          const neighborKey = `${row}-${col}`;
+        for (const neighbor of graph[currentNode] || []) {
+          const [neighborRow, neighborCol] = neighbor.split("-");
+          const neighborKey = `${neighborRow}-${neighborCol}`;
           const distance = distances[currentNode] + 1;
           if (distance < distances[neighborKey]) {
             distances[neighborKey] = distance;
@@ -134,12 +138,26 @@ const DijkstraAnimatedView = () => {
     dijkstra();
   };
 
-  const displayGrid = gridArray.map((arrayItem) => {
-    const [row, col] = arrayItem.coordinates;
+  const renderGrid = () => {
+    return (
+      <div
+        className="grid"
+        style={{
+          gridTemplateColumns: `repeat(${dimension}, minmax(0, 1fr))`,
+        }}
+      >
+        {createCells}
+      </div>
+    );
+  };
+
+  const createCells = gridArray.map((cell) => {
+    const [row, col] = cell.coordinates;
     const isStart = row === startSquare[0] && col === startSquare[1];
     const isEnd = row === endSquare[0] && col === endSquare[1];
     const isVisited = visitedNodes.has(`${row}-${col}`);
     const isInShortestPath = shortestPath.includes(`${row}-${col}`);
+    const isObstacle = cell.type === "obstacle";
 
     let cellClassName = "bg-gray-400 border-2 aspect-square";
     if (isStart) cellClassName = "bg-red-400 border-2 aspect-square";
@@ -148,35 +166,51 @@ const DijkstraAnimatedView = () => {
       cellClassName = "bg-yellow-300 border-2 aspect-square";
     if (isInShortestPath && !isStart && !isEnd)
       cellClassName = "bg-green-400 border-2 aspect-square";
+    if (isObstacle) cellClassName = "bg-slate-900 border-2 aspect-square";
 
     return (
-      <div
+      <button
         key={`${row}-${col}`}
         className={cellClassName}
         onClick={() => handleCellClick(row, col)}
-      ></div>
+      ></button>
     );
   });
 
   const handleCellClick = (row, col) => {
     if (row === startSquare[0] && col === startSquare[1]) return;
     if (row === endSquare[0] && col === endSquare[1]) return;
-    const updatedGrid = gridArray.map((item) => {
-      if (item.coordinates[0] === row && item.coordinates[1] === col) {
+    const updatedGrid = gridArray.map((cell) => {
+      const [cellRow, cellCol] = cell.coordinates;
+      if (cellRow === row && cellCol === col) {
         return {
-          ...item,
-          type: item.type === "obstacle" ? "cell" : "obstacle",
+          ...cell,
+          type: cell.type === "obstacle" ? "cell" : "obstacle",
         };
       }
-      return item;
+      return cell;
     });
     setGridArray(updatedGrid);
+    renderGrid();
   };
 
+  const handleChangeDimension = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const newDimension = data.get("dimension");
+    setDimension(newDimension);
+    const newGrid = createGridArray(newDimension, newDimension);
+    setGridArray(newGrid);
+
+    setVisitedNodes(new Set());
+    setShortestPath([]);
+
+    renderGrid();
+  };
 
   return (
     <div className="mx-auto w-1/2">
-      <div className={`grid grid-cols-[repeat(30,minmax(0,1fr))]`}>{displayGrid}</div>
+      {renderGrid()}
       <button
         className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
         onClick={handleVisualizeClick}
@@ -186,9 +220,13 @@ const DijkstraAnimatedView = () => {
           ? "Animating Path..."
           : "Visualize Dijkstra's Algorithm"}
       </button>
-      <label htmlFor="dimensions"></label>
-        <input type="number" onChange={e=>setDimension(e.target.value)}/>
-      <button type="button" onClick={()=>createGridArray(dimension,dimension)}>change dimensions</button>
+
+      <form onSubmit={handleChangeDimension}>
+        <input type="number" name="dimension" />
+        <button type="submit">change dimensions</button>
+      </form>
+
+      <form action=""></form>
     </div>
   );
 };
