fbf9f3b55e8f91985ba233c4608ec00e975e0451
adrian.taay <adrian.tm@live.com>
2024-05-17 20:10:51 +0800

    testing

--- src/pages/SelectionSort/SelectionSort.view.jsx
+++ src/pages/SelectionSort/SelectionSort.view.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./SelectionSort.module.css";
 import { motion } from "framer-motion";
 
@@ -28,44 +28,119 @@ function bubbleSort(arr) {
 }
 
 function SelectionSort() {
-  const [array, setArray] = useState([6, 42, 5, 10, 10, 3, 27, -1, 29]);
+  const [array, setArray] = useState([6, 42, 5, 10, 3, 27, -1, 29]);
   const [ind1, setInd1] = useState(-1);
   const [ind2, setInd2] = useState(-1);
   const [lowest, setLowest] = useState(null);
-  const [lowestIndex, setLowestIndex] = useState(null);
+  const [indexOfLowest, setIndexOfLowest] = useState(null);
+  const [iteration, setIteration] = useState(null);
+  const [commence, setCommence] = useState(false);
+
+  // proceeds to the next iteration by incrementing the previous ind1 by 1 so that the arranged digits will not be iterated again.
+  useEffect(() => {
+    if (!commence) {
+      return;
+    }
+
+    if (iteration >= array.length - 1) {
+      setCommence(false);
+      return;
+    }
+
+    setInd1(iteration);
+    setInd2(iteration + 1);
+    setLowest(array[iteration]);
+    setIndexOfLowest(iteration);
+  }, [iteration]);
 
   function start() {
-    setInd1(0);
-    setInd2(1);
-    setLowest(array[0]);
+    setCommence(true);
+    setIteration(0);
   }
 
-  console.log(lowest);
-
   function compare() {
+    // const copy = [...array];
+
+    // if (ind2 >= array.length) {
+    //   return;
+    // }
+
+    // if (copy[ind2] <= lowest) {
+    //   setLowest(copy[ind2]);
+    //   setIndexOfLowest(ind2);
+    // }
+
+    // if (ind2 === array.length - 1) {
+    //   let temp = copy[ind1];
+    //   copy[ind1] = copy[indexOfLowest];
+    //   copy[indexOfLowest] = temp;
+
+    //   setArray(copy);
+    //   setIteration((i) => i + 1);
+    // } else {
+    //   setInd2((i) => i + 1);
+    // }
     const copy = [...array];
 
-    if (copy[ind2] < lowest) {
+    // if (
+    //   ind1 === array.length - 2 &&
+    //   ind2 === array.length - 1 &&
+    //   copy[ind2] <= lowest
+    // ) {
+    //   let temp = copy[ind1];
+    //   copy[ind1] = copy[indexOfLowest];
+    //   copy[indexOfLowest] = temp;
+    //   setCommence(false);
+    //   return;
+    // }
+
+    if (ind2 >= array.length) {
+      return;
+    }
+
+    if (ind1 === array.length - 2 && ind2 === array.length - 1) {
+      if (copy[ind2] <= lowest) {
+        let temp = copy[ind1];
+        copy[ind1] = copy[indexOfLowest];
+        copy[indexOfLowest] = temp;
+      }
+    }
+
+    // // if array[index 2] is lower than or equal to the current lowest, set it as the new lowest and store its index then proceed to next element. if array[index 2] is greater, just proceed to next element.
+    if (copy[ind2] <= lowest) {
       setLowest(copy[ind2]);
-      setLowestIndex(ind2);
+      setIndexOfLowest(ind2);
       setInd2((i) => i + 1);
     } else {
       setInd2((i) => i + 1);
     }
 
+    // if index 2 is at the last element and that element is greater than or equal to the lowest,
     if (ind2 === array.length - 1 && copy[ind2] >= lowest) {
       let temp = copy[ind1];
-      copy[ind1] = copy[lowestIndex];
-      copy[lowestIndex] = temp;
-      setInd1((i) => i + 1);
-      setInd2(ind1 + 1);
-      setLowest(array[ind1]);
-      setLowestIndex(null);
+      copy[ind1] = copy[indexOfLowest];
+      copy[indexOfLowest] = temp;
+
+      setTimeout(() => {
+        setIteration((i) => i + 1);
+      }, 500);
     }
+
     setArray((a) => [...copy]);
   }
-  // console.log(`array: ${array}`, `ind1: ${ind1}`, `ind2: ${ind2}`);
-  // console.log(`result: ${result}`);
+
+  useEffect(() => {
+    console.log(
+      "array",
+      array,
+      `ind1: ${ind1}`,
+      `ind2: ${ind2}`,
+      "lowest",
+      lowest,
+      "indexOfLowest",
+      indexOfLowest
+    );
+  }, [ind1, ind2]);
 
   return (
     <section className={style.section}>
@@ -74,7 +149,7 @@ function SelectionSort() {
         {array.map((item, index) => {
           return (
             <motion.div
-              key={`${item}` + `${index}`}
+              key={item}
               layout
               transition={spring}
               className={
@@ -87,7 +162,7 @@ function SelectionSort() {
         })}
       </div>
       <div style={{ marginTop: "16px" }}>
-        {!lowest ? "Press Start" : `Lowest: ${lowest}`}
+        {!commence ? "Press Start" : `Lowest: ${lowest}`}
       </div>
       <div className={style.container}>
         <button className={style.button} onClick={() => start()}>
