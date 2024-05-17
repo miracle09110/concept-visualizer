973d852cccec89fa5fee0c514c280f2de1c9b241
adrian.taay <adrian.tm@live.com>
2024-05-17 20:38:36 +0800

    refactored landing page to accommodate route chilren and Outlet

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App";
 import "./index.css";
 import ExploreController from "./pages/landingPage/explore/Explore.controller";
 import BubbleSortController from "./pages/BubbleSort/BubbleSort.controller";
+import ServerClientAnimation from "./pages/ServerClient/ServerClient.view";
 
 const router = createBrowserRouter([
   {
@@ -26,7 +27,7 @@ const router = createBrowserRouter([
       { path: "/a-star", element: null },
       { path: "/state-props", element: null },
       { path: "/use-context", element: null },
-      { path: "/server-client", element: null },
+      { path: "/server-client", element: <ServerClientAnimation /> },
       { path: "/rest-api", element: null },
       { path: "/webhook", element: null },
     ],
